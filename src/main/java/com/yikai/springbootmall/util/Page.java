9239276bfd5475c0867c9e9698421eb32a0dea9a
Ken <yikaichen@systex.com>
2022-11-22 14:35:21 +0800

    商品功能 - 實作查詢商品列表(分頁 + 總筆數)

---
+++ src/main/java/com/yikai/springbootmall/util/Page.java
@@ -0,0 +1,42 @@
+package com.yikai.springbootmall.util;
+
+import java.util.List;
+
+public class Page<T> {
+    private Integer limit;
+    private Integer ofset;
+    private Integer total;
+    private List<T> results;
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Integer limit) {
+        this.limit = limit;
+    }
+
+    public Integer getOfset() {
+        return ofset;
+    }
+
+    public void setOfset(Integer ofset) {
+        this.ofset = ofset;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public List<T> getResults() {
+        return results;
+    }
+
+    public void setResults(List<T> results) {
+        this.results = results;
+    }
+}

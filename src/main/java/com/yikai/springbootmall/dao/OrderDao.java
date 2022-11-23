6b003733e4ee2a2d276dbdfdad6e7671d0180a3e
Ken <yikaichen@systex.com>
2022-11-23 16:01:29 +0800

    訂單功能 - 創建訂單(上)

---
+++ src/main/java/com/yikai/springbootmall/dao/OrderDao.java
@@ -0,0 +1,12 @@
+package com.yikai.springbootmall.dao;
+
+import com.yikai.springbootmall.model.OrderItem;
+
+import java.util.List;
+
+public interface OrderDao {
+
+    Integer createOrder(Integer userId, Integer totalAmount);
+
+    void createOrderItems(Integer orderId, List<OrderItem> orderItemList);
+}
